75ec09f476dcdfba74a66726599f58bd5e2f3d9b
Kieran0507 <k.goodinson94@gmail.com>
2021-12-02 15:16:04 +0000

    made it get data

---
+++ groupminiproject/src/components/DataGetter.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const DataGetter = () => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        axios.get("http://api.disneyapi.dev/characters").then((result) => {
+            
+            setData(result.data.data)
+        }, (error) => {
+            
+            setError(error);
+        })
+    }, [])
+
+    if (error) {
+        return <div>
+            <h1>Big Fat Error : {error.message}</h1>
+        </div>
+    } else {
+        console.log(data);
+       return <h2>Data Got</h2>
+    }
+   
+}
+ 
+export default DataGetter;
