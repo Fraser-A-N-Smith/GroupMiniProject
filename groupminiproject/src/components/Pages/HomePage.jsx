a8453f760ff4f0d616a952e71263633a49b9968c
sarahkgh <sarahk.work@outlook.com>
2021-12-02 15:32:14 +0000

    front end pages

---
+++ groupminiproject/src/components/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Image from "./Image";
+
+
+const HomePage = () => {
+    return ( 
+        <div>
+            <Header/>
+            <Image/>
+        </div>
+     );
+}
+ 
+export default HomePage;
---
+++ groupminiproject/src/components/Pages/Image.jsx
@@ -0,0 +1,10 @@
+const Image = () => {
+    return ( 
+<>
+        <img id="img1" src="https://www.pngall.com/wp-content/uploads/2018/05/Disney-Logo-PNG-Pic.png"></img>
+</>
+     );
+}
+ 
+export default Image;
+
