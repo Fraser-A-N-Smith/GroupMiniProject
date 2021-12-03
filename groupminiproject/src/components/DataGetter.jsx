88fec0778e2bdada0aa753f0eacdc222521b33ae
Kieran0507 <k.goodinson94@gmail.com>
2021-12-03 09:23:47 +0000

    made it not work

--- groupminiproject/src/components/DataGetter.jsx
+++ groupminiproject/src/components/DataGetter.jsx
@@ -1,18 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import DisneyList from "./DisneyList";
 
 const DataGetter = () => {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
     useEffect(() => {
         axios.get("http://api.disneyapi.dev/characters").then((result) => {
-            
+            printData(result.data.data)
             setData(result.data.data)
         }, (error) => {
             
             setError(error);
         })
     }, [])
+    const printData = (data) => {
+        
+        data.list.map((data) => {
+           
+            if (data.films[0] !== undefined) {
+                console.log(data.films[0]);
+                return <DisneyList list={data.films}/>
+            }
+        })
+    }
 
     if (error) {
         return <div>
@@ -20,7 +31,7 @@ const DataGetter = () => {
         </div>
     } else {
         console.log(data);
-       return <h2>Data Got</h2>
+       return <DisneyList list={data}/>
     }
    
 }
---
+++ groupminiproject/src/components/DisneyList.jsx
@@ -0,0 +1,26 @@
+const DisneyList = (data) => {
+    
+    // const filterQuery = (query) => (list) => list.title.includes(query);
+ 
+    // let filteredList = [];
+    
+    // if (completed) {
+    //   filteredList = list.filter(filterQuery(query))
+    // }
+    // else if(notCompleted) {
+    //   filteredList = list.filter(filterQuery(query))
+    // }
+    // else{
+    //   filteredList = list.filter(filterQuery(query))
+    // }
+    
+    // console.log(data);
+    
+      
+    // printData();
+    return (<div>
+        <ul></ul>
+    </div> );
+}
+ 
+export default DisneyList;
